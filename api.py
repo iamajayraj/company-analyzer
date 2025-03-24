c31806c24c2518f8712889dcbae807ab4534aa4e
iamajayraj <iamajayrajrathore@gmail.com>
2025-03-24 20:24:02 +0530

    First Commit

---
+++ api.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+from utils import get_links, get_article_content, get_llm_response, get_sentiment_distribution, get_topic_overlap, \
+    get_comparative_analysis, get_final_sentiment
+import os
+from ast import literal_eval
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+
+# Load API keys from environment variables
+load_dotenv()
+SERPER_API_KEY = os.getenv('SERPER_API_KEY')
+GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+HF_TOKEN = os.getenv("HF_TOKEN")
+
+
+@app.route('/query', methods=['POST'])
+def query():
+    # get the data/query from streamlit app
+    company = request.get_json()['query']
+    links = get_links(company)
+    articles = [get_article_content(link) for link in links]
+    articles_json = [get_llm_response(article) for article in articles]
+    data = {'Company Name': company, 'Articles': articles_json}
+    sentiment_counts = get_sentiment_distribution(articles_json)
+    data["Comparative Sentiment Score"] = {"Sentiment Distribution": sentiment_counts}
+    common_topics = get_topic_overlap(articles_json)
+    data["Topic Overlap"] = {"Common Topics": common_topics}
+    data["Coverage Differences"] = get_comparative_analysis(articles_json)
+    data["Final Sentiment Analysis"] = get_final_sentiment(articles_json)
+
+    return jsonify(data)
+
+
+app.run(host='localhost', port=5001, debug=True)
---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import requests
+from gtts import gTTS
+#from googletrans import Translator
+from translate import Translator
+
+
+def text_to_speech(text, output_file="output.mp3"):
+    # Convert text to speech
+    translator = Translator(from_lang='en',to_lang='hi')
+    print(text)
+    print(type(text))
+    translation = translator.translate(text)
+    print(translation)
+    speech = gTTS(text=translation, lang='hi')
+    speech.save(output_file)
+
+
+st.title("News Summarization and Text-to-Speech Application")
+
+company_name = st.text_input('Enter Company Name')
+if st.button("Enter"):
+    # Make a POST request to the Flask API
+    url = "http://localhost:5001/query"
+    response = requests.post(url, json={'query': company_name})
+
+    # Return of flask
+    if response:
+        text_to_speech(response.json()["Final Sentiment Analysis"])
+        st.audio("output.mp3", format='audio/mp3')
+        st.json(response.json())
